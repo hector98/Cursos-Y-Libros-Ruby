b24a8d640bb097ff876204645f8788b0d38b4674
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-01-02 19:56:38 -0600

    Comenzar a organizar ejercicios de ruby, y seguimieto de un curso de udemy

---
+++ CursoUdemy/Cap1GettingStarted/the_p_method.rb
@@ -0,0 +1,11 @@
+puts "Hector Manuel"
+p "Hector Manuel"
+
+puts 5
+puts "5"
+
+p 5
+p "5"
+
+puts "Hi there\nline break"
+p "Hi there\nline break"
---
+++ CursoUdemy/Cap1GettingStarted/the_puts_method.rb
@@ -0,0 +1,18 @@
+puts 1
+puts 1 + 2
+puts(1 + 2)
+
+puts "Hello World!"
+puts "I'am alive and well!"
+
+puts 3.14159
+
+puts
+
+puts "Afterwards"
+
+puts 5
+puts 3
+puts 5
+
+puts 5, 3, 5
