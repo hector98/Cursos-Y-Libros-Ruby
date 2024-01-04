41e74927cf4be8b778c9d4bb77299dbf1792f930
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-01-03 22:19:16 -0600

    Comenzar capitulo 2(variables) del curso de Udemy

---
+++ CursoUdemy/Cap2Variables/declaring_variables.rb
@@ -0,0 +1,24 @@
+age = 25 #Declaring a variable
+puts age
+
+name = "Hector"
+last_name = "Barrios"
+puts name 
+puts last_name
+
+puts age + 7 # 25 + 7
+puts name + last_name
+
+age = 26
+puts age
+
+age_in_ien_years = age + 10 # 36
+puts age_in_ien_years
+
+age = age + 7 # 33
+puts age
+
+chamelon = 14
+chamelon = "Some random text"
+chamelon = 3.14
+puts chamelon
