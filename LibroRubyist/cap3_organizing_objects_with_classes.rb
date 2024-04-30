9a70ccf97c5815b1e66b6a9a1c6b28b452437745
Hector Manuel Barrios Barrios <barrioshector13@gmail.com>
2024-04-29 18:45:54 -0600

    ejercicios capitulo 3

--- LibroRubyist/cap3_organizing_objects_with_classes.rb
+++ LibroRubyist/cap3_organizing_objects_with_classes.rb
@@ -20,3 +20,42 @@ class C
   end
 end
 C.new.m
+
+# Instance variables and object state
+# Listing 3.1 An instance variable maintaining its value between methods calls.
+class Person 
+  def set_name(string)
+    puts "Setting person's name..."
+    @name = string
+  end
+  
+  def get_name
+    @name
+  end
+end
+hector = Person.new
+hector.set_name("Hector")
+puts hector.get_name
+
+# Initializing an object with state
+class Ticket
+  def initialize(venue, date)
+    #puts "Creating a new ticket!"
+    @venue = venue
+    @date = date
+  end
+
+  def venue
+    @venue
+  end
+
+  def date
+    @date
+  end
+end
+th = Ticket.new("Town Hall", "2013-11-12")
+cc = Ticket.new("Convetion Center", "2014-02-13")
+puts "We've created two tickets."
+puts "The first is for a #{th.venue} even on #{th.date}."
+puts "The second is for an event on #{cc.venue} at #{cc.date}."
+
